539ded8faa79eb219a91b9ce011c9896eb2616b9
Greggles <gregory@wetransfer.com>
2022-10-25 23:48:13 +0200

    Add toon pack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,26 +12,41 @@ public class PlayerController : MonoBehaviour
     Quaternion targetRotation;
 
     bool isRunning = false;
+    bool inAction;
 
     Transform camera;
 
     Animator animator;
+
+    CharacterController characterController;
 
     private void Awake()
     {
         camera = Camera.main.transform;
         animator = GetComponent<Animator>();
 
+        characterController = GetComponent<CharacterController>();
+
     }
 
     private void Update()
     {
+        if (inAction)
+        {
+            return;
+        }
+
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
         if (Input.GetButtonDown("Run"))
         {
             isRunning = !isRunning;
+        }
+
+        if (Input.GetButtonDown("Roll"))
+        {
+            StartCoroutine(DoAction("Roll"));
         }
 
         float moveAmount = Mathf.Clamp01(Mathf.Abs(h) + Mathf.Abs(v));
@@ -41,7 +56,7 @@ public class PlayerController : MonoBehaviour
         var moveDir = Quaternion.Euler(0, camYRotation, 0) * moveInput;
         float moveSpeed = isRunning ? runSpeed : walkSpeed;
 
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        characterController.Move(moveDir * moveSpeed * Time.deltaTime);
 
         if (moveAmount > 0)
         {
@@ -51,5 +66,28 @@ public class PlayerController : MonoBehaviour
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, angularSpeed * Time.deltaTime);
 
         animator.SetFloat("moveAmount", moveAmount * moveSpeed / runSpeed, 0.2f, Time.deltaTime);
+    }
+
+    IEnumerator DoAction(string animName)
+    {
+        inAction = true;
+        animator.CrossFade(animName, 0.2f);
+        yield return null;
+
+        var animState = animator.GetNextAnimatorStateInfo(0);
+
+        float timer = 0f;
+
+        while (timer <= animState.length)
+        {
+            timer += Time.deltaTime;
+
+            if(animator.IsInTransition(0) && timer > 0.4f){
+                break;
+            }
+            yield return null;
+        }
+   
+        inAction = false;
     }
 }
